507d86f52527f586410dcb481251ad92686a4806
user.name <chuan.ma@qq.com>
2017-03-25 15:13:03 +0800

    add new files

---
+++ install_package.R
@@ -0,0 +1,17 @@
+wants <- c("knitr","rmarkdown","devtools","epicalc","mosaic",
+           "showtext","pander","PerformanceAnalytics","fitdistrplus",
+           "CircStats","MASS","mixtools","boot","TrialSize","vcd",
+           "ggplot2","pspearman","gvlma","car","lmtest","leaps",
+           "plyr","bootstrap","elrm","rms","Deducer","bestglm",
+           "survival","robust","mlogit","nnet","VGAM","ordinal",
+           "Sample.Size","phia","mvtnorm","pscl","mosaic","XML",
+           "pipeR","Rcmdr","rgl","HH","DescTools","multcomp",
+           "effects","sandwich","qcc","devtools","ggmap","mosaic",
+           "Hmisc","pastecs","psych","doBy","gmodels","CircStats",
+           "expm","koRpus","ldbounds","ggm","coin","DescTools",
+           "Rcurl","maptools","rgdal","animation","leaflet",
+           "polycor","pROC","rms","pgirmess","rateratio.test",
+           "exactci","Deducer","VGAM","ordinal","AER","gplots",
+           "AICcmodavg")
+has <- wants %in% rownames(installed.packages())
+if(any(!has)) install.packages(wants[!has])
