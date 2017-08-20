f2d475f3fa146aa26d62742b4af6a13b4a06c4bc
user.name <chuan.ma@qq.com>
2017-08-20 16:41:09 +0800

    forpic

--- R_books/data_collection_with_R.R
+++ R_books/data_collection_with_R.R
@@ -16,8 +16,7 @@ mac_path <- '/Users/machuan/CodeSpace/Code.Data/R'
 data_path <- ifelse(Sys.info()[1]=='Windows', windows_path, mac_path)
 
 # 第一章 概述---------------------------------------------------
-setwd(file.path(data_path,
-                '/基于R语言的自动数据收集/ch-1-introduction'))
+
 #url = 'http://en.wikipedia.org/wiki/List_of_World_Heritage_in_Danger'
 #heritage_parsed <- htmlParse(url, encoding = 'utf-8')
 
@@ -36,4 +35,65 @@ danger_table_clean <- danger_table %>%
          yins=as.numeric(yins),
          yend=as.numeric(str_replace(yend, '.*(\\d{4}$)', '\\1')))
 
+reg_y <- "[/][ -]*[[:digit:]]*[.]*[[:digit:]]*[;]"
+reg_x <- "[;][ -]*[[:digit:]]*[.]*[[:digit:]]*"
+y_coords <- str_extract(danger_table$locn, reg_y)
+y_coords <- str_sub(y_coords, 3, -2) %>%
+  as.numeric() %>% round(2)
+x_coords <- str_extract(danger_table$locn, reg_x)
+x_coords <- str_sub(x_coords, 3, -1) %>%
+  as.numeric() %>% round(2)
+danger_table_clean$y_coords <- y_coords
+danger_table_clean$x_coords <- x_coords
+danger_table_clean$locn <- NULL
 
+pch <- ifelse(danger_table_clean$crit=='nat', 19, 2)
+map('world', col='darkgrey', lwd=1,
+    mar=c(0.1, 0.1, 0.1, 0.1))
+points(danger_table_clean$x_coords, danger_table_clean$y_coords,
+       pch=pch)
+box()
+
+# 第二章 HTML-----------------------------------------------------
+setwd(file.path(data_path,
+                '/基于R语言的自动数据收集/ch-2-html'))
+parsed_fortunes <- htmlParse('fortunes.html')
+
+h1 <- list('body'=function(x){NULL})
+parsed_fortunes1 <- htmlTreeParse('fortunes.html',
+                                  handlers = h1,
+                                  asTree = T)
+
+h2 <- list(
+  startElement = function(node, ...){
+    name <- xmlName(node)
+    if(name %in% c('div', 'title')){NULL}
+    else{node}
+  },
+  comment = function(node) {NULL}
+)
+
+parsed_fortunes2 <- htmlTreeParse('fortunes.html',
+                                  handlers = h2,
+                                  asTree = T)
+
+geItalics <- function(){
+  i_container = character()
+  list(i = function(node, ...){
+    i_container <<- c(i_container, xmlValue(node))
+  },
+  return_I = function() i_container)
+}
+h3 <- geItalics()
+invisible(htmlTreeParse('fortunes.html', handlers = h3))
+h3$return_I()
+
+
+# 第三章 XML and JSON---------------------------------------------
+setwd(file.path(data_path,
+                '/基于R语言的自动数据收集/ch-3-xml'))
+
+bond <- xmlParse('bond.xml')
+root <- xmlRoot(bond)
+xmlName(root)
+xmlSize(root)
